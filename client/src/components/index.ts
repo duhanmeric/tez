a278c59976d731363a501615c9601b72b104fdc8
cince1 <107986392+cince1@users.noreply.github.com>
2022-11-15 15:54:48 +0300

    SbError created

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,3 +2,4 @@ export { default as SbButton } from "./SbButton/SbButton";
 export { default as SbContainer } from "./SbContainer/SbContainer";
 export { default as SbFileUpload } from "./SbFileUpload/SbFileUpload";
 export { default as SbFileItem } from "./SbFileItem/SbFileItem";
+export { default as SbError } from "./SbError/SbError";
