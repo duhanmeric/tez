2826115508baf73e03723f1a13eca3695346c8b0
cince1 <107986392+cince1@users.noreply.github.com>
2022-11-16 12:11:03 +0300

    video file is ready to be zipped

--- server/controllers/video_controller.py
+++ server/controllers/video_controller.py
@@ -8,10 +8,16 @@ import os
 def main(last_video_path, filename, video_extension):
     videoClip = VideoFileClip(last_video_path)
 
+    # tmp_videos path'inden mp3 dosyası olusturma
     getAudioPath = registerAudio(filename)
     audioClip = videoClip.audio
     audioClip.write_audiofile(getAudioPath)
 
- 
-    audioClip.close()
+    # ilerde videoya entegre ilerde basilacak kelimeler simdilik bos
+    words = []
+
+    # uzerine kelimeler eklenmis video dosyası ve ileride ziplenecek dosya
+    # outputs'a kaydediliyor
+    videoClip.write_videofile(os.path.join(get_save_folders(
+        "outputs"), f"{filename+video_extension}"), logger=None)
     videoClip.close()
